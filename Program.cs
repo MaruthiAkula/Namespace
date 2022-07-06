46e4a126c9adad51528b15aacf72b019a3967916
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-06 11:44:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Namespace.namespace1;
+//using Namespace.namespace2;
+//using A = Namespace.namespace1;
+//using B=Namespace.namespace2;
+
+
+
+namespace Namespace
+{
+    namespace namespace1
+    {
+        public class ClassA
+        {
+            public static void print()
+            {
+                Console.WriteLine("I'm ClassA in namespace1");
+            }
+        }
+    }
+
+}
+namespace Namespace
+{
+    namespace namespace2
+    {
+        public class ClassB
+        {
+            public static void print()
+            {
+                Console.WriteLine("I'm ClassB in namespace2");
+            }
+        }
+    }
+}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       Namespace.namespace1. ClassA.print();
+       Namespace.namespace2.ClassB.print();
+        }
+    }
+
